0ce6b58bd4b8f3be27304c9e41372dad96b54b8f
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-15 20:07:36 +0200

    Fix country names in main function initialization

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::io::*;
 use std::io;
 fn main() {
     let mut finland = Country::new("Finland", &5600000, &900000, &vec![], &false);
-    let mut sweden = Country::new("Finland", &10000000, &200000, &vec![], &false);
-    let mut norway = Country::new("Finland", &5500000, &100000, &vec![], &false);
-    let mut denmark = Country::new("Finland", &6000000, &50000, &vec![], &false);
+    let mut sweden = Country::new("Sweden", &10000000, &200000, &vec![], &false);
+    let mut norway = Country::new("Norway", &5500000, &100000, &vec![], &false);
+    let mut denmark = Country::new("Denmark", &6000000, &50000, &vec![], &false);
 
     println!("| 1) Finland | 2) Sweden | 3) Norway | 4) Denmark |");
     let mut choice = String::new();

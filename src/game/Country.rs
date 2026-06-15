ddff1d1cd1b246802d39b7258f8082e33d34bfe6
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-15 18:58:43 +0200

    Initial commit

---
+++ src/game/Country.rs
@@ -0,0 +1,31 @@
+pub struct Country {
+    name: String,
+    population: i64,
+    army_size: i64,
+    conquered_countries: Vec<String>,
+    is_conquered: bool,
+}
+
+impl Country {
+    pub fn new(name: &str, population: &i64, army_size: &i64, conquered_countries: &Vec<String>, is_conquered: &bool) -> Self {
+        Self {
+            name: name.to_string(),
+            population: *population,
+            army_size: *army_size,
+            conquered_countries: conquered_countries.to_vec(),
+            is_conquered: *is_conquered,
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        return self.name.clone();
+    }
+
+    pub fn get_population(&self) -> i64 {
+        return self.population;
+    }
+
+    pub fn get_army_size(&self) -> i64  {
+        return self.army_size;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+pub mod game;
+use crate::game::Country::*;
+use std::io::*;
+use std::io;
+fn main() {
+    let mut finland = Country::new("Finland", &5600000, &900000, &vec![], &false);
+    let mut sweden = Country::new("Finland", &10000000, &200000, &vec![], &false);
+    let mut norway = Country::new("Finland", &5500000, &100000, &vec![], &false);
+    let mut denmark = Country::new("Finland", &6000000, &50000, &vec![], &false);
+
+    println!("| 1) Finland | 2) Sweden | 3) Norway | 4) Denmark |");
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("Error reading input.");
+    let choice = choice.trim();
+    println!("Choose your country:");
+    match choice {
+        "1" => {
+            let name = finland.get_name();
+            let population = finland.get_population();
+            let army = finland.get_army_size();
+            println!("Country: {}", name);
+            println!("Population: {}", population);
+            println!("Army size: {}", army);
+        }
+        "2" => {
+            let name = sweden.get_name();
+            let population = sweden.get_population();
+            let army = sweden.get_army_size();
+            println!("Country: {}", name);
+            println!("Population: {}", population);
+            println!("Army size: {}", army);
+        }
+        "3" => {
+            let name = norway.get_name();
+            let population = norway.get_population();
+            let army = norway.get_army_size();
+            println!("Country: {}", name);
+            println!("Population: {}", population);
+            println!("Army size: {}", army);
+        }
+        "4" => {
+            let name = denmark.get_name();
+            let population = denmark.get_population();
+            let army = denmark.get_army_size();
+            println!("Country: {}", name);
+            println!("Population: {}", population);
+            println!("Army size: {}", army);
+        }
+        _ => {
+            println!("Wrong input");
+        }
+    }
+}

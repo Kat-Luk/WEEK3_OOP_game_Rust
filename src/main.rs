6d07348ceb566b07be3fefaa8944a02e2b94c587
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-18 15:48:18 +0200

    Initialize countries in main function for player selection

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ use crate::game::GameMap::*;
 use std::io::*;
 use std::io;
 fn main() {
-    
     let player;
+    let mut finland = Country::new(String::from("Finland"), 5600000, 900000, vec![], false);
+    let mut sweden = Country::new(String::from("Sweden"), 10000000, 200000, vec![], false);
+    let mut norway = Country::new(String::from("Norway"), 5500000, 100000, vec![], false);
+    let mut denmark = Country::new(String::from("Denmark"), 6000000, 50000, vec![], false);
+
     println!("| 1) Finland | 2) Sweden | 3) Norway | 4) Denmark |");
     println!("Choose your country: ");
     let mut choice = String::new();
